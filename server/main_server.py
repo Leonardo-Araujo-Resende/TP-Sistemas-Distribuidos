96b46d44b7d129aa3e8fe3b84ccea705c51e4f22
JhonataMiranda <jhonata.miranda@ufv.br>
2024-08-08 21:21:12 -0300

    upd(+): corrigindo erro do sql

--- server/main_server.py
+++ server/main_server.py
@@ -7,20 +7,17 @@ def main():
     bd_conn = sqlite3.connect('corrida_maluca.db')
     c = bd_conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT NOT NULL,
                 password TEXT NOT NULL
                 )""")
     
     c.execute("""CREATE TABLE IF NOT EXISTS cards (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT NOT NULL,
                 filename TEXT NOT NULL,
                 FOREIGN KEY(username) REFERENCES users(username)
                 )""")
     
     c.execute("""CREATE TABLE IF NOT EXISTS decks (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT NOT NULL,
                 deck_id INTEGER NOT NULL,
                 card_filename TEXT NOT NULL,
