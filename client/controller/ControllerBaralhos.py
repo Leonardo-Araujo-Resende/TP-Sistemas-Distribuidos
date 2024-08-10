3aaab7d2f0a22ad8496b05050a321923c65f045d
JhonataMiranda <jhonata.miranda@ufv.br>
2024-08-10 20:27:51 -0300

    upd(+): fix bug ao resetar deck

--- client/controller/ControllerBaralhos.py
+++ client/controller/ControllerBaralhos.py
@@ -38,4 +38,4 @@ class ControllerBaralhos():
                 if card.get_id() == card_in_collection.get_id():
                     card_in_collection.increment_quantity()
                     break
-
+        self.decks[deck_index].reset_deck()                
