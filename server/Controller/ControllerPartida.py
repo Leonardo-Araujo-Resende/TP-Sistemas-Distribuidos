41317b149971106ca77711d61d534ff5273ec2ec
JhonataMiranda <jhonata.miranda@ufv.br>
2024-08-11 14:08:50 -0300

    upd(+): controller partida servidor

--- server/Controller/ControllerPartida.py
+++ server/Controller/ControllerPartida.py
@@ -10,3 +10,15 @@ class ControllerPartida():
         self.jogador3 = None
         self.deck3 = None
         self.all_cards = all_cards
+    
+    def game_start(self,):
+
+        mao_player1 = self.deck1[0:2]
+        mao_player2 = self.deck2[0:2]
+        mao_player3 = self.deck3[0:2]
+        
+        self.jogador1.sendall(f"{mao_player1}".encode("utf8"))
+        self.jogador2.sendall(f"{mao_player2}".encode("utf8"))
+        self.jogador3.sendall(f"{mao_player3}".encode("utf8"))
+
+    
